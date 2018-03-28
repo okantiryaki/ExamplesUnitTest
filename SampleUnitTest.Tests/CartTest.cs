56ceec578731c62b06b619be02c5ac72e5bd5f41
Okan Tiryaki <okantiryaki@outlook.com>
2018-03-28 22:59:23 +0300

    [Test - Urun Ekleme] - Ürün ekleme test'i yazıldı

---
+++ SampleUnitTest.Tests/CartTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleUnitTest.Tests
+{
+    [TestClass]
+    public class CartTest
+    {
+        [TestMethod]
+        public void sepete_urun_eklenebilir()
+        {
+            // Arrange
+            // bu değer fonksiyonumuz eğer doğru çalışır ise bize dönücek olan değer
+            const int beklenen = 1;
+
+            var cartManager = new CartManager();
+            var cartItem = new CartItem
+            {
+                Product = new Product
+                {
+                    ProductId = 1,
+                    ProductName = "Table",
+                    ProductPrice = 120
+                },
+                Quantity = 1
+            };            
+            
+            //Act Olay
+            cartManager.Add(cartItem);
+            var toplamElamanSayisi = cartManager.TotalItems;
+
+            // burada karşılaştırma yapıyoruz.
+            Assert.AreEqual(beklenen, toplamElamanSayisi);
+        }
+    }
+}
---
+++ SampleUnitTest/CartManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleUnitTest
+{
+    public class CartManager
+    {
+
+        // ihtiyaçlarım nelerdir ?
+        // sepete ürün eklenebilir olması lazım
+        // sepetten ürün çıkartılabilir olması lazım
+        // sepet temizlenebilir olması lazım
+
+        private readonly List<CartItem> _cartItems;
+
+        public CartManager()
+        {
+            _cartItems = new List<CartItem>();
+        }
+
+        public void Add(CartItem cartItem)
+        {
+            _cartItems.Add(cartItem);
+        }
+
+
+        public void Remove(int productId)
+        {
+            var product = _cartItems.FirstOrDefault(x=> x.Product.ProductId == productId);
+            _cartItems.Remove(product);
+        }
+
+        public List<CartItem> CartItems
+        {
+            get
+            {
+                return _cartItems;
+            }
+        }
+
+        public void Clear()
+        {
+            _cartItems.Clear();
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                return _cartItems.Sum(x => x.Product.ProductPrice);
+            }
+        }
+
+        public int TotalQuantity
+        {
+            get
+            {
+                return _cartItems.Sum(x => x.Quantity);
+            }
+        }
+
+        public int TotalItems
+        {
+            get
+            {
+                return _cartItems.Count();
+            }
+        }
+    }
+}
